b6dc62a79e6306ea1e152ba42929ee4480582ca6
Dudimetla Sai <saiy63001@gmail.com>
2025-08-15 15:08:29 +0530

    Time: 3 ms (59.72%) | Memory: 53.4 MB (84.87%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        List<List<Integer>> ls=new ArrayList<>();
+        int[] outdegree=new int[n+1];
+        int[] indegree=new int[n+1];
+        for(int i=1;i<=n;i++){
+            ls.add(new ArrayList<>());
+        }
+      for(int i=0;i<trust.length;i++){
+        outdegree[trust[i][0]]++;
+        indegree[trust[i][1]]++;
+      }  
+      for(int i=1;i<=n;i++){
+        if(outdegree[i]==0 && indegree[i]==n-1){
+            return i;
+        }
+      }
+return -1;
+    }
+}

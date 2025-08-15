6417c3b5746b738ca02b7498a511dd3c23abf34f
Dudimetla Sai <saiy63001@gmail.com>
2025-08-15 13:37:37 +0530

    Time: 5 ms (76.96%) | Memory: 45.6 MB (93.09%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int n=numCourses;
+        List<List<Integer>> ls=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        int[] res=new int[n];
+        int j=0;
+        int[] indegree=new int[n];
+      for(int i=0;i<n;i++){
+        ls.add(new ArrayList<>());
+      }  
+      for(int i=0;i<prerequisites.length;i++){
+        ls.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        indegree[prerequisites[i][0]]++;
+      }
+      for(int i=0;i<n;i++){
+        if(indegree[i]==0){
+            list.add(i);
+        }
+      }
+      
+      for(int i=0;i<list.size();i++){
+         for(int v:ls.get(list.get(i))){
+            indegree[v]--;
+            if(indegree[v]==0){
+               list.add(v);
+            }
+         }
+      }
+      if(list.size()!=n){
+    return new int[]{};
+      }
+     
+      for(int i=0;i<list.size();i++){
+        res[j++]=list.get(i);
+      }
+      
+return res;
+    }
+}
